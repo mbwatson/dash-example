a2d0287727a02c53f657331733162733c52c9c7d
Matt Watson <mwatson@renci.org>
2025-01-15 15:49:40 -0500

    add github link
    add gh link
    
    trim css

--- src/components/navbar.py
+++ src/components/navbar.py
@@ -1,8 +1,17 @@
 from dash import html
+from dash_iconify import DashIconify
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 from src.components.drawer import drawer
 from src.components.theme_toggle import theme_toggle
+
+github_link = dmc.Anchor(
+  DashIconify(icon="feather:github", width=20, color="var(--mantine-color-text)"),
+  variant="transparent",
+  id="github-link",
+  href="https://github.com/mbwatson/dash-example/",
+  target="_blank",
+)
 
 logo = dmc.Anchor(
   "[LOGO]",
@@ -22,7 +31,7 @@ nav_items = [
 menu = dmc.Flex(
   [navlink(label=item["label"], href=item["href"]) for item in nav_items],
   wrap="no-wrap",
-  className="navbar--menu",
+  className="navbar--menu-items",
   justify="flex-end"
 )
 
@@ -32,7 +41,6 @@ navbar = dmc.Paper(
   children=[
     drawer,
     logo,
-    dmc.Flex([menu, theme_toggle])
-  ],
-)
+    dmc.Flex([menu, github_link, theme_toggle], className="navbar--menu")
+  ])
 
