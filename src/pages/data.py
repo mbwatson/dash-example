62c5cecd6b4326ffe356cbdc3f3e61643baa7e26
Matt Watson <mwatson@renci.org>
2025-01-16 09:32:40 -0500

    render simple histogram & table
    render hist & table

--- src/pages/data.py
+++ src/pages/data.py
@@ -1,7 +1,41 @@
-from dash import html
+from dash import callback, dash_table, dcc, html, Input, Output
+import dash_mantine_components as dmc
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('./data/data-goldstandard/rsvnet_hospitalization.csv')
 
 header = html.H1('Data')
 
 layout = html.Div([
   header,
+  dmc.Stack([
+    dcc.Graph(figure=px.histogram(df, x='date', y='value'), id="my-plot"),
+    dash_table.DataTable(
+      data=df.to_dict('records'),
+      page_size=20,
+      id="my-table"
+    ),
+  ])
 ])
+
+@callback(
+  Output("my-table", "data"),
+  [Input("my-plot", "clickData")]
+)
+def on_trace_click(click_data):
+    """Listen to click events and update table, passing filtered rows"""
+    p = trace_click['points'][0]
+
+    # here, use 'customdata' property of clicked point, 
+    # could also use 'curveNumber', 'pointIndex', etc.
+    if 'customdata' in p:
+        key = p['customdata']['my-key']
+
+    df_f = get_corresponding_rows(df, key)
+
+    return df_f.to_dict('records')
+
+def get_corresponding_rows(df, my_key):
+    """Filter df, return rows that match my_key"""
+    return df[df['key-column'] == my_key]
