3876e855fb6bcab3cad9de7b8ee590b0930cebcf
Matt Watson <mwatson@renci.org>
2025-01-17 11:30:08 -0500

    fixes for csv & parquet poc data pages
    fix fn name
    
    include data, wire up
    
    rm obsolete page

--- src/pages/data_csv.py
+++ src/pages/data_csv.py
@@ -18,24 +18,3 @@ layout = html.Div([
   header,
   dmc.Stack([chart, table])
 ])
-
-@callback(
-  Output("my-table", "data"),
-  [Input("my-plot", "clickData")]
-)
-def on_trace_click(click_data):
-  """Listen to click events and update table, passing filtered rows"""
-  p = trace_click['points'][0]
-
-  # here, use 'customdata' property of clicked point, 
-  # could also use 'curveNumber', 'pointIndex', etc.
-  if 'customdata' in p:
-    key = p['customdata']['my-key']
-
-  df_f = get_corresponding_rows(df, key)
-
-  return df_f.to_dict('records')
-
-def get_corresponding_rows(df, my_key):
-  """Filter df, return rows that match my_key"""
-  return df[df['key-column'] == my_key]
